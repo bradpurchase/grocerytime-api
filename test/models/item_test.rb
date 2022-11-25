ab51334dae7e2053c5c63e346a6f893620e3cc41
Brad Purchase <5716+bradpurchase@users.noreply.github.com>
2022-11-25 08:31:28 -0500

    wip: item classification business logic

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -22,20 +22,22 @@ class ItemTest < ActiveSupport::TestCase
   end
 
   describe "before_save" do
-    test "parses an item name with no embedded quantity" do
-      assert_equal item_name, item.name
-      assert_equal 1, item.quantity
-    end
-
-    describe "quantity embedded in item name" do
-      let(:item_name) { "Egg Whites x 2" }
-      let!(:item) do
-        trip.items.create!(user: user, category: category, name: item_name)
+    describe "parse_item_name" do
+      test "parses an item name with no embedded quantity" do
+        assert_equal item_name, item.name
+        assert_equal 1, item.quantity
       end
 
-      test "correctly parses an item name with embedded quantity" do
-        assert_equal "Egg Whites", item.name
-        assert_equal 2, item.quantity
+      describe "quantity embedded in item name" do
+        let(:item_name) { "Egg Whites x 2" }
+        let!(:item) do
+          trip.items.create!(user: user, category: category, name: item_name)
+        end
+
+        test "correctly parses an item name with embedded quantity" do
+          assert_equal "Egg Whites", item.name
+          assert_equal 2, item.quantity
+        end
       end
     end
   end
