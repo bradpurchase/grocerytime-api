142ea1fc30dead2a909fa731a8bb4432a495d14e
Brad Purchase <5716+bradpurchase@users.noreply.github.com>
2022-11-26 17:57:32 -0500

    feat: update Meal model

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,4 +2,13 @@ class Meal < ApplicationRecord
   belongs_to :recipe
   belongs_to :user
   belongs_to :store
+
+  has_many :users, class_name: MealUser.to_s, dependent: :destroy
+  has_many :items, class_name: Item.to_s
+
+  after_destroy :disassociate_items
+
+  def disassociate_items
+    items.update_all(meal_id: nil)
+  end
 end
