73e2347b8e63769469b54b62823851d771c4a1fb
Brad Purchase <5716+bradpurchase@users.noreply.github.com>
2022-12-11 17:17:57 -0500

    chore: allow *.ngrok.io in config.hosts

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module Grocerytime
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.hosts << /.*\.ngrok\.io/
   end
 end
